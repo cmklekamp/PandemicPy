5acbd20ab56c3b5155db82d83d819138fcbeec79
Jacob Gregie <mastermind583@gmail.com>
2021-11-23 21:07:34 -0500

    "coordinates suck" - Jacob

--- Board.py
+++ Board.py
@@ -36,60 +36,48 @@ class BoardFrame(Frame):
         img = Image.open("Resources/MapFinal.png")
         resized_image= img.resize((1050,600), Image.ANTIALIAS)
         new_image= ImageTk.PhotoImage(resized_image)
-        label = Label(self,image = new_image)
-        label.photo = new_image
-        label.place(x=0, y=0)
+        background = Label(self,image = new_image)
+        background.photo = new_image
+        background.place(x=0, y=0)
         
-        
-        self.names = ["Bogotá","Johannesburg","Buenos Aires","Mexico City","Lima","Los Angeles","Miami",
-                 "Kinshasa","São Paulo","Santiago","Khartoum","Lagos","Istanbul","Kolkata",
-                 "Tehran","Cairo","Algiers","Moscow","Chennai","Karachi","Delhi","Mumbai",
-                 "Riyadh","Baghdad","San Francisco","Atlanta","Madrid","New York","Essen","Chicago",
-                 "St. Petersburg","Montréal","London","Paris","Milan","Washington","Tokyo","Beijing",
-                 "Shanghai","Bangkok","Manila","Ho Chi Minh City","Sydney","Seoul","Taipei","Jakarta","Osaka","Hong Kong",]
+        self.names = ["Bogotá","Johannesburg","Buenos Aires","Mexico City","Lima","Los Angeles","Miami","Kinshasa",
+                      "São Paulo","Santiago","Khartoum","Lagos","Istanbul","Kolkata","Tehran","Cairo",
+                      "Algiers","Moscow","Chennai","Karachi","Delhi","Mumbai","Riyadh","Baghdad",
+                      "San Francisco","Atlanta","Madrid","New York","Essen","Chicago","St. Petersburg","Montréal",
+                      "London","Paris","Milan","Washington","Tokyo","Beijing","Shanghai","Bangkok",
+                      "Manila","Ho Chi Minh City","Sydney","Seoul","Taipei","Jakarta","Osaka","Hong Kong",]
 
-        coordinates = [(272,364),(575,460),(318,488),(197,320),(263,414),(141,272),(254,300),(537,391),
-                       (354,451),(282,485),(587,332),(502,360),()]
+        self.coordinates = [(272,364),(575,460),(318,488),(197,320),(263,414),(141,272),(254,300),(537,391),
+                            (354,451),(282,485),(587,332),(502,360),(577,244),(753,310),(644,265),(584,286),
+                            (501,262),(604,178),(730,339),(690,303),(721,290),(708,321),(631,304),(623,273),
+                            (128,258),(242,272),(480,247),(275,245),(514,200),(232,242),(582,155),(273,226),
+                            (488,198),(501,214),(519,227),(261,255),(908,266),(836,249),(852,281),(789,337),
+                            (850,335),(809,347),(940,486),(869,259),(852,303),(809,398),(891,269),(830,311)]
+
         self.buttons = list()
-     
-        button_img = Image.open("Resources/black.png")
-        #resized_image= button_img.resize((17,17))
-        new_image= ImageTk.PhotoImage(button_img)
-        new_button = Button(self, image=new_image, borderwidth=0, command=lambda: self.button_click(self.names[0]))
-        new_button.image = new_image
-        x_coordinate,y_coordinate = coordinates[12]
-        new_button.place(height = 17, width = 17, x=x_coordinate, y=y_coordinate)
-
         
-        # for i in range(48):
-        #     if (i < 12):
-        #         path = "Resources/yellow.png"
-        #     elif (i >= 12 and i < 24):
-        #         path = "Resources/black.png"
-        #     elif (i >= 24 and i < 36):
-        #         path = "Resources/blue.png"
-        #     else:
-        #         path = "Resources/red.png"
-               
-        #     # new_button = Button(self, command=lambda i = i: self.button_click(self.names[i]))
-        #     # x_coordinate,y_coordinate = coordinates[0]
-        #     # self.buttons.append(new_button)
-        #     # self.buttons[i].place(x=x_coordinate*i, y=y_coordinate*i)
+        # create buttons for each city
+        for i in range(48):
+            if (i < 12):
+                path = "Resources/yellow.png"
+            elif (i >= 12 and i < 24):
+                path = "Resources/black.png"
+            elif (i >= 24 and i < 36):
+                path = "Resources/blue.png"
+            else:
+                path = "Resources/red.png"
 
-        #     button_img = Image.open(path)
-        #     #resized_image= button_img.resize((17,17))
-        #     new_image= ImageTk.PhotoImage(button_img)
-        #     new_button = Button(self, image=new_image, borderwidth=0, command=lambda i = i: self.button_click(self.names[i]))
-        #     new_button.image = new_image
-        #     x_coordinate,y_coordinate = coordinates[0]
-        #     self.buttons.append(new_button)
-        #     self.buttons[i].place(height = 17, width = 17, x=x_coordinate, y=y_coordinate)
-
-        
+            button_img = Image.open(path)
+            new_image= ImageTk.PhotoImage(button_img)
+            new_button = Button(self, image=new_image, borderwidth=0, command=lambda i = i: self.button_click(self.names[i]))
+            new_button.image = new_image
+            
+            x_coordinate,y_coordinate = self.coordinates[i]
+            self.buttons.append(new_button)
+            self.buttons[i].place(height = 17, width = 17, x=x_coordinate, y=y_coordinate)       
     
     def button_click(self, name):
-        print(name)
-        
+        print(name)        
 
 # Main routing for testing MenuFrame
 if __name__ == "__main__":
